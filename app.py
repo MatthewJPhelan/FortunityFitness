e2d4d5bbeac02543f6dd6355d34036134708e666
Matthew Phelan <matthew.phelanonline@gmail.com>
2020-03-28 19:02:36 +0000

    Initial setup 2

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import time
 
 import redis
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./Project/Pages')
 cache = redis.Redis(host='redis', port=6379)
 
 
@@ -18,8 +18,19 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
+def init():
+    count = get_hit_count()
+    return 'I have been seen {} times.\n'.format(count)
 
 @app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Hello Boooiiiiiii! I have been seen {} times.\n'.format(count)
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
+
